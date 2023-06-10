25d965b0e94b303f9ad9d0e0dcec2c08b715df80
Juno144 <pawelekstanley@gmail.com>
2023-06-11 01:18:40 +0200

    not happy

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.Scanner;
-import java.util.function.Function;
 
 public class Main {
    public static void main(String[] args) {
